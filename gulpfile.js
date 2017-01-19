fa581359aac976bcc1c3ac8f336b22cbbfaf5c96
Jinbo Wang <jinbwan@microsoft.com>
2017-01-19 11:00:44 +0800

    Refine gulpfile logic

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,15 @@ const vsce = require('vsce');
 var git = require('git-rev-sync');
 const cliArgs = require('yargs').argv;
 const azure = require('azure-storage');
-const glob = require('glob');
-const async = require('async');
 
 function getVsixName() {
     const packageJson = JSON.parse(fs.readFileSync('package.json'));
     let vsixName = `${packageJson.name}-${packageJson.version}.vsix`;
-    if (!cliArgs.tag) {
-        if (/^\d+$/.test(cliArgs.pullRequest)) {
-            vsixName = `${packageJson.name}-${packageJson.version}-PR${cliArgs.pullRequest}.vsix`
-        } else if (cliArgs.branch) {
-            vsixName = `${packageJson.name}-${packageJson.version}-${cliArgs.branch}-${git.short()}.vsix`
+    if (!process.env.TRAVIS_TAG) {
+        if (/^\d+$/.test(process.env.TRAVIS_PULL_REQUEST)) {
+            vsixName = `${packageJson.name}-${packageJson.version}-PR#${process.env.TRAVIS_PULL_REQUEST}.vsix`
+        } else if (process.env.TRAVIS_BRANCH) {
+            vsixName = `${packageJson.name}-${packageJson.version}-${process.env.TRAVIS_BRANCH}-${git.short()}.vsix`
         } else {
             vsixName = `${packageJson.name}-${packageJson.version}-${git.short()}.vsix`;
         }
@@ -35,33 +33,22 @@ gulp.task('package', (done) => {
 
 gulp.task('upload', (done) => {
     if (!cliArgs.azureConnectionString) {
-        done('Missing connection string parameter.');
+        done('Missing azure connection string parameter.');
         return ;
     }
     const blobSvc = azure.createBlobService(decodeURIComponent(cliArgs.azureConnectionString));
-    const container = cliArgs.tag ? 'vscode-arduino' : 'vscode-arduino-devint';
+    const container = process.env.TRAVIS_TAG ? 'vscode-arduino' : 'vscode-arduino-devint';
     blobSvc.createContainerIfNotExists(container, {publicAccessLevel: 'blob'}, (error, result, response) => {
         if (error) {
             done(`Create container failed with error "${error}"`);
         } else {
-            glob("**/*.vsix", (error, files) => {
+            const vsixFile = getVsixName();
+            blobSvc.createBlockBlobFromLocalFile(container, path.basename(vsixFile), vsixFile, (error, result, response) => {
                 if (error) {
-                    done(error);
-                    return;
+                    done(`Failed to upload vsix file "${vsixFile}" to azure blob. See error message "${error}"`);
+                } else {
+                    done();
                 }
-                const tasks = [];
-                files.forEach(file => {
-                    tasks.push(callback => {
-                        blobSvc.createBlockBlobFromLocalFile(container, path.basename(file), file, (error, result, response) => {
-                            if (error) {
-                                callback(`Upload binary file "${file}" to azure blob failed.`);
-                            } else {
-                                callback();
-                            }
-                        });
-                    });
-                });
-                async.series(tasks, done);
             });
         }
     });
